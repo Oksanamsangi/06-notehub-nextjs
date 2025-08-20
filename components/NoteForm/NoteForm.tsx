4f77b189e0e813ae7e62b6dd91ab3404019ec2e9
Oksanamsangi <162930811+Oksanamsangi@users.noreply.github.com>
2025-08-19 23:52:24 -0600

    fix

--- components/NoteForm/NoteForm.tsx
+++ components/NoteForm/NoteForm.tsx
@@ -94,7 +94,7 @@ export default function NoteForm({onClose}: NoteFormProps) {
             name="content"
             component="span"
             className={styles.error}
-            lalal
+            
           />
         </div>
 
